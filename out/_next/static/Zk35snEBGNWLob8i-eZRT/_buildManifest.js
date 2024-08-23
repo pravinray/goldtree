b6ccc90e44a718c3a5b5924c6de30f5e111e0fdf
Pravin Ray <pravin@Pravins-MacBook-Pro.local>
2024-08-23 17:21:54 +0545

    added build files to deploy

---
+++ out/_next/static/Zk35snEBGNWLob8i-eZRT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/961-5420f0189efc760f.js","static/chunks/pages/index-cf04c8332ec0b537.js"],"/_error":["static/chunks/pages/_error-d20dd478b791b37a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
